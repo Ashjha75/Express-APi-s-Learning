79a21f11a8b1feb3af7bc1de19c01c6bc14c7991
AshishJha <ajha5645@gmail.com>
2023-09-01 12:48:54 +0530

    4th commit

--- Api1/routers/routes.ts
+++ Api1/routers/routes.ts
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { signup } = require("../controllers/Auths/sign-up")
+const { signin } = require("../controllers/Auths/sign-in")
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 
 module.exports = router
