3eb54caed84223129b6280954e106fbac9b0f5a0
AshishJha <ajha5645@gmail.com>
2023-10-24 12:42:34 +0530

    Tuesday commit

---
+++ Api2/config/connectDb.ts
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+export const dbConnect = async () => {
+    mongoose.set('strictQuery', true)
+    let isConnected = false;
+    if (!process.env.DATABASE_URL) {
+        console.log("DATABASE_URL is Missing")
+        return;
+    }
+    if (isConnected) {
+        console.log("Already connected to Db");
+        return;
+    }
+    try {
+        mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        });
+        isConnected = true
+        console.log("SuccesFully Connected to Db");
+
+    } catch (error: any) {
+        console.log("Error while connected to Database")
+
+    }
+
+
+}
--- Api2/index.ts
+++ Api2/index.ts
@@ -1,6 +1,28 @@
+
+// express imports 
 const express = require("express");
 const app = express();
 
+// database connect
+import { dbConnect } from "./config/connectDb";
+// .env imports
 require("dotenv").config();
+const { PORT, DTABASE_URL } = process.env;
+console.log(PORT)
+// middleware imports
+const cookiParser = require("cookie-parser");
+
+// routes imports
 
 console.log("first")
+
+const start = async () => {
+    try {
+        await dbConnect();
+        app.listen((PORT || 8000), () => { console.log(`app is listening on Port ${PORT}`) })
+
+    } catch (error) {
+        console.log("Issue while starting app")
+    }
+}
+start();
