c9161cc0feca237fbbdcbe6eb90ee7ad1b4708c5
AshishJha <ajha5645@gmail.com>
2023-10-29 11:38:37 +0530

    Sunday commit

--- Api2/controllers/authControllers.ts
+++ Api2/controllers/authControllers.ts
@@ -1,194 +1,196 @@
-import { hashedPassword, isLoggedIn, setcookies } from "../libs/actions/commonFunction";
+import {
+  hashedPassword,
+  isLoggedIn,
+  setcookies,
+} from "../libs/actions/commonFunction";
 import { Request, Response } from "express";
 import User from "../models/User";
-const bcryptjs = require("bcryptjs")
+const bcryptjs = require("bcryptjs");
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { commonservices } from "../libs/service/commonservices";
 require("dotenv").config();
 import { BadRequestError } from "../libs/errors/badrequest.error";
-const commonServices = new commonservices()
+const commonServices = new commonservices();
 type decoded = {
-    id: string,
-    email: string,
-    username: string,
-    role: string
-}
+  id: string;
+  email: string;
+  username: string;
+  role: string;
+};
 
 // signup function
 export const register = async (req: Request, res: Response) => {
-    try {
-        // get data from requests body
-        const { username, email, password } = req.body;
-        // validate the data
-        if (!username || !email || !password) {
-            return res.status(500).json({
-                success: false,
-                message: "Please fill all required field"
-            })
-        }
-        // check if email already exists
-        const isEmailAlreadyExists = await User.findOne({ email });
-        const isUsernameAlreadyExists = await User.findOne({ username });
-        if (isEmailAlreadyExists) {
-            return res.status(500).json({
-                success: false,
-                message: "Email already exists"
-            })
-        }
-        if (isUsernameAlreadyExists) {
-            return res.status(500).json({
-                success: false,
-                message: "Username already exists"
-            })
-        }
-        // hashed password 
-        const hashedpass = await hashedPassword(password);
-        const newUser = new User({ username, email, password: hashedpass });
-        const newsavedUser = await newUser.save()
-
-        return res.status(200).json({
-            success: true,
-            message: "User created successfully.",
-            data: newsavedUser
-        })
-    } catch (error: any) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        })
-
+  try {
+    // get data from requests body
+    const { username, email, password } = req.body;
+    // validate the data
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all required field",
+      });
     }
-}
+    // check if email already exists
+    const isEmailAlreadyExists = await User.findOne({ email });
+    const isUsernameAlreadyExists = await User.findOne({ username });
+    if (isEmailAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists",
+      });
+    }
+    if (isUsernameAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Username already exists",
+      });
+    }
+    // hashed password
+    const hashedpass = await hashedPassword(password);
+    const newUser = new User({ username, email, password: hashedpass });
+    const newsavedUser = await newUser.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "User created successfully.",
+      data: newsavedUser,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Login function
 export const login = async (req: Request, res: Response) => {
-    try {
-        // request body
-        const { email, password } = req.body;
-        // validate the req body data
-        if (!email || !password) {
-            return res.status(401).json({
-                success: false,
-                message: "Please fill all required field."
-            })
-            // throw new BadRequestError("Please provide email and password");
-        }
-        // check email
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(402).json({
-                success: false,
-                message: "User doesn't exists."
-            })
-        }
-        const payload = {
-            id: user._id,
-            email: user.email,
-            username: user.username,
-            role: user.roles
-        }
-
-        // comare the password and generate token
-        if (await bcryptjs.compare(password, user.password)) {
-            let token = jwt.sign(payload, process.env.JWT_SECRET!, {
-                expiresIn: "2h",
-            });
-            user.token = token;
-            user.password = undefined;
-            // set the global user details
-
-            // set token in cookies
-            setcookies(token, res);
-            res.status(200).json({
-                success: true,
-                message: "User loggedin successfully",
-                data: user
-            });
-        }
-        else {
-            return res.status(402).json({
-                success: false,
-                message: "Invalid credentials."
-            })
-        }
-    } catch (error: any) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        })
+  try {
+    // request body
+    const { email, password } = req.body;
+    // validate the req body data
+    if (!email || !password) {
+      return res.status(401).json({
+        success: false,
+        message: "Please fill all required field.",
+      });
+      // throw new BadRequestError("Please provide email and password");
     }
-}
+    // check email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(402).json({
+        success: false,
+        message: "User doesn't exists.",
+      });
+    }
+    const payload = {
+      id: user._id,
+      email: user.email,
+      username: user.username,
+      role: user.roles,
+    };
+
+    // comare the password and generate token
+    if (await bcryptjs.compare(password, user.password)) {
+      let token = jwt.sign(payload, process.env.JWT_SECRET!, {
+        expiresIn: "2h",
+      });
+      user.token = token;
+      user.password = undefined;
+      // set the global user details
+
+      // set token in cookies
+      setcookies(token, res);
+      res.status(200).json({
+        success: true,
+        message: "User loggedin successfully",
+        data: user,
+      });
+    } else {
+      return res.status(402).json({
+        success: false,
+        message: "Invalid credentials.",
+      });
+    }
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // logout function
 export const logout = async (req: Request, res: Response) => {
-    try {
-        res.cookie("token", "", {
-            httpOnly: true,
-            expires: new Date(Date.now()),
-        })
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    });
 
-        return res.status(200).json({
-            success: true,
-            message: "User logged out successfully"
-        })
-    } catch (error: any) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+    return res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // reset password function
 export const resetPassword = async (req: Request, res: Response) => {
-    try {
-        const userLoggedIn = await isLoggedIn(req, res);
-        if (!userLoggedIn) {
-            // get data from request body
-            const { email, password, newPassword } = req.body;
-            // validate the data
-            if (!email || !password || !newPassword) {
-                return res.status(402).json({
-                    success: false,
-                    message: "Please fill all required field."
-                })
-            }
-            // check email
-            const user = await User.findOne({ email });
-            if (!user) {
-                return res.status(402).json({
-                    success: false,
-                    message: "User doesn't exists."
-                })
-            }
-            if (await bcryptjs.compare(newPassword, user.password)) {
-                return res.status(402).json({
-                    success: false,
-                    message: "New password cannot be same as old password."
-                })
-            }
-            // comare the password and generate token
-            if (await bcryptjs.compare(password, user.password)) {
-                // hashed password
-                const hashedpass = await hashedPassword(newPassword);
-                user.password = hashedpass;
-                await user.save()
-                return res.status(200).json({
-                    success: true,
-                    message: "Password reset successfully",
-                });
-            }
-        }
-        else {
-            return res.status(402).json({
-                success: false,
-                message: "User not logged in."
-            })
-        }
-    } catch (error: any) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        })
+  try {
+    console.log("helloji...😎");
+    const userLoggedIn = await isLoggedIn(req, res);
+    if (!userLoggedIn) {
+      // get data from request body
+      const { email, password, newPassword } = req.body;
+      // validate the data
+      if (!email || !password || !newPassword) {
+        return res.status(402).json({
+          success: false,
+          message: "Please fill all required field.",
+        });
+      }
+      // check email
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(402).json({
+          success: false,
+          message: "User doesn't exists.",
+        });
+      }
+      if (await bcryptjs.compare(newPassword, user.password)) {
+        return res.status(402).json({
+          success: false,
+          message: "New password cannot be same as old password.",
+        });
+      }
+      // comare the password and generate token
+      if (await bcryptjs.compare(password, user.password)) {
+        // hashed password
+        const hashedpass = await hashedPassword(newPassword);
+        user.password = hashedpass;
+        await user.save();
+        return res.status(200).json({
+          success: true,
+          message: "Password reset successfully",
+        });
+      }
+    } else {
+      return res.status(402).json({
+        success: false,
+        message: "User not logged in.",
+      });
     }
-}
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- Api2/index.ts
+++ Api2/index.ts
@@ -1,7 +1,6 @@
-
-// express imports 
+// express imports
 const express = require("express");
-require('express-async-errors');
+require("express-async-errors");
 const app = express();
 
 // database connect
@@ -13,26 +12,47 @@ const { PORT } = process.env;
 // middleware imports
 const cookiParser = require("cookie-parser");
 import bodyParser from "body-parser";
+import { logger } from "./middlewares/logEvents.middleware";
+const cors = require("cors");
 
 // routes imports
-const authRouter = require("./routes/authRouter")
-
-// middlewares used 
+const authRouter = require("./routes/authRouter");
+// middlewares used
+app.use(logger);
+const whitelist = [
+  "http://localhost:8080",
+  "http://127.0.0.1:8080",
+  "https://www.google.com/",
+];
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  exposedHeaders: ["set-cookie"],
+  optionsSuccessStatus: 200,
+};
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookiParser());
 
-
-app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/auth", authRouter);
 
 const start = async () => {
-    try {
-        await dbConnect();
-        app.listen((PORT || 8000), () => { console.log(`app is listening on Port ${PORT}`) })
-
-    } catch (error) {
-        console.log("Issue while starting app")
-    }
-}
+  try {
+    await dbConnect();
+    app.listen(PORT || 8001, () => {
+      console.log(`app is listening on Port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Issue while starting app");
+  }
+};
 start();
