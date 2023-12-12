9db2c3f48b0fe26225ad488b0761d6447fe6a6e8
AshishJha <ajha5645@gmail.com>
2023-12-12 13:33:53 +0530

    authentication contollers updates

--- Api3/controllers/auth/authentication.controllers.ts
+++ Api3/controllers/auth/authentication.controllers.ts
@@ -5,7 +5,7 @@ import Jwt, { JwtPayload } from 'jsonwebtoken';
 import { ApiError } from "../../utils/errors/ApiError";
 import { asyncHandler } from '../../utils/errors/Asynchandler.errors';
 import { Roles } from '../../constants';
-import { emailVerificationMailgenContents, sendMail } from '../../utils/mails/sendMail.utils';
+import { emailVerificationMailgenContents, forgotPasswordMailgenContents, sendMail } from '../../utils/mails/sendMail.utils';
 import { AuthRequest } from '../../../Api2/libs/service/interfaces/Request_Response.interface';
 import path from 'path';
 export const generateAccessAndRefreshToken = async (userId: any) => {
@@ -46,7 +46,7 @@ const register = asyncHandler(async (req: Request, res: Response) => {
         userName,
         email,
         password,
-        roles: roles || Roles[0],
+        roles: roles || Roles.User,
     });
     /**
   * unHashedToken: unHashed token is something we will send to the user's mail
@@ -131,7 +131,7 @@ const verifyEmail = asyncHandler(async (req: Request, res: Response) => {
 
     await User.findByIdAndUpdate(hashedToken.id, { isEmailVerified: true });
 
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'email.html'), (err) => {
+    res.sendFile(path.join(__dirname, '../../utils/public/email.html'), (err) => {
         if (err) {
             throw new ApiError(400, "Something went wrong while sending the email", []);
         }
@@ -164,4 +164,124 @@ const resendEmailVerification = asyncHandler(async (req: AuthRequest, res: Respo
 
 })
 // refresh token
-export { register, login, logout, verifyEmail, resendEmailVerification };   
+const refreshToken = asyncHandler(async (req: Request, res: Response) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    if (!incomingRefreshToken) {
+        throw new ApiError(400, "Refresh Token is missing.", []);
+    }
+    const decodeToken = Jwt.verify(incomingRefreshToken, process.env.JWT_REFRESH_TOKEN_SECRET!) as JwtPayload;
+    const user = await User.findById(decodeToken?.id);
+    if (!user) {
+        throw new ApiError(400, "Invalid refresh token", []);
+    }
+
+    if (incomingRefreshToken !== user.refreshToken) {
+        throw new ApiError(400, "Refresh token is expired or used", []);
+    }
+    const { refreshToken, accessToken } = await generateAccessAndRefreshToken(user._id);
+    const options = {
+        httOnly: true,
+        secure: process.env.NODE_ENV === "PROD",
+        expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+    }
+    res.status(200).
+        cookie("refreshToken", refreshToken, options).
+        cookie("accessToken", accessToken, options).
+        json({
+            success: true,
+            message: "Successfully refreshed the token.",
+            data: { accessToken, refreshToken }
+        })
+})
+// change password
+const changePassword = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const { oldPassword, newPassword } = req.body;
+    if (!oldPassword || !newPassword) {
+        throw new ApiError(400, "Please fill all required credentilas.")
+    }
+    const currentUser = await User.findById(req?.user?.id);
+    if (!currentUser) {
+        throw new ApiError(400, "User not found", []);
+    }
+    const isPasswordCorrect = await currentUser.validatePassword(oldPassword);
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Password is incorrect", []);
+    }
+    currentUser.password = newPassword;
+    currentUser.save();
+    res.status(200).json({
+        success: true,
+        message: "Password updated successfully"
+    }
+    )
+});
+// forgot password
+const forgotPasswordRequest = asyncHandler(async (req: Request, res: Response) => {
+    const email = req.body.email;
+    if (!email) {
+        throw new ApiError(400, "Email is required", []);
+    }
+    const isExistingUser = await User.findOne({ email });
+    if (!isExistingUser) {
+        throw new ApiError(400, "User not exists", []);
+    }
+    const unhashedToken = await isExistingUser.generateTemporaryToken();
+
+    await sendMail({
+        email: email,
+        subject: "Password Reset Request",
+        mailgenContent: forgotPasswordMailgenContents(isExistingUser.userName,
+            `${req.protocol}://${req.get(
+                "host"
+            )}/api/v1/auth/reset-password/${unhashedToken}`
+        )
+    })
+    res.status(200).json({
+        success: true,
+        message: "Password reset link has been sent to your email.",
+    })
+})
+// reset forgot password
+const resetForgotPassword = asyncHandler(async (req: Request, res: Response) => {
+    const token = req.params.token;
+    const { newPassword } = req.body;
+    if (!token) {
+        throw new ApiError(400, "Invalid token", []);
+    }
+    if (!newPassword) {
+        throw new ApiError(400, "Please provide new password", []);
+    }
+    const hashedToken = await Jwt.verify(token, process.env.JWT_EMAIL_TOKEN_SECRET!) as JwtPayload;
+    const isExistingUser = await User.findById(hashedToken.id);
+    if (!isExistingUser) {
+        throw new ApiError(400, "User not exists", []);
+    }
+    isExistingUser.password = newPassword;
+    await isExistingUser.save();
+    res.status(200).json({
+        success: true,
+        message: "Password changed successfully",
+    })
+})
+// assign role
+const assignRole = asyncHandler(async (req: Request, res: Response) => {
+    const { userName, role } = req.body;
+    if (!userName || !role) {
+        throw new ApiError(400, "Please provide all credentials", []);
+    }
+    const currentUser = await User.findOne({ userName });
+    if (!currentUser) {
+        throw new ApiError(400, "User not exists", []);
+    }
+    if (!Object.values(currentUser.roles).includes(role)) {
+        throw new ApiError(400, `Invalid role: ${role}`, []);
+    }
+    currentUser.roles = role;
+    currentUser.save();
+    res.status(200).json({
+        success: true,
+        message: "Role Assigned Successfully"
+    })
+})
+
+export { register, login, logout, verifyEmail, resendEmailVerification, refreshToken, changePassword, forgotPasswordRequest, resetForgotPassword, assignRole };   
